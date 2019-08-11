7adaaed269b9c5bbbbd950ba32de75907a4b5148
Francisco Sandoval <francisco@mtblc.co>
2019-08-11 13:18:28 -0400

    refactor store (singleton)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import configureStore, { history } from './store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
 
-ReactDOM.render(<App store={store} history={history} />, document.getElementById('root'));
+ReactDOM.render(
+  <App store={store} history={history} />,
+  document.getElementById('root'),
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from './ducks';
 
 export const history = createBrowserHistory();
 
+const middleware = [
+  thunkMiddleware,
+  routerMiddleware(history),
+];
+
+let store;
+
 export default function configureStore(initialState) {
+  if (store) return store;
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const middleware = applyMiddleware(routerMiddleware(history), thunk);
-  return createStore(rootReducer(history), initialState, composeEnhancers(middleware));
+  store = createStore(
+    rootReducer(history),
+    initialState,
+    composeEnhancers(applyMiddleware(...middleware))
+  );
+  return store;
 }
